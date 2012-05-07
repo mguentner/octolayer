7f26e0cf6c878f87b82abcfb577f0bf7e19a7f2e
Maximilian Güntner <maximilian.guentner@gmail.com>
2012-05-07 13:12:13 +0200

    first version of OctoLayer Signed-off-by: Maximilian Güntner <maximilian.guentner@gmail.com>

---
+++ install/plugins/octolayer.rb
@@ -0,0 +1,54 @@
+# Title: OpenLayers tag for Jekyll/Octopress
+# Author: Maximilian Güntner <maximilian.guentner@gmail.com>
+# Description: Embed beautfiul maps in your pages
+#
+# Syntax:  {% map lat lon zoomlevel markerTitle* markerDescription* %} - * => optional
+#
+
+module Jekyll
+  class OctoLayer < Liquid::Tag
+    # Class variable for creating unique mapIDs
+    @@mapCount = 0
+    @lat = nil
+    @lon = nil
+    @title = "" 
+    @description = ""
+    @zoomLevel = nil
+    @mapID = nil
+    def initialize(tag_name, markup, tokens)
+      if markup =~ /(\d{1,3}\.\d+)(\s+)(\d{1,3}\.\d+)(\s+)(\d+)(\s+)(\"(([^\"])*)\")?(\s+)(\"(([^\"])*)\")?(\s*)/i
+        @lat=$1
+        @lon=$3
+        @zoomLevel=$5
+        @title = $8        
+        @description = $12  
+        if @title
+          @title = @title.gsub(/[']/, "\\\\'")
+        end
+        if @description
+          @description = @description.gsub(/[']/, "\\\\'")
+        end
+        @mapID = @@mapCount + 1
+        @@mapCount += 1  
+      end
+      super
+    end
+    def render(context)
+      output = super
+      if @lat
+        caption = "Map Data: <a href='http://openstreetmap.org'>OpenStreeMap</a> and contributors. <a href='http://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a> - Tiles Courtesy of <a href='http://www.mapquest.com/' target='_blank'>MapQuest</a><img src='http://developer.mapquest.com/content/osm/mq_logo.png'>"
+        source = "<div class='map'><div style='height:420px' id='map#{@mapID}'></div><div class='mapCaption'>#{caption}</div></div>"\
+                 "<script type='text/javascript'>"\
+                 "jQuery(document).ready(function() {"\
+                 "initmap('map#{@mapID}',#{@lat},#{@lon},#{@zoomLevel},'#{@title}','#{@description}');"\
+                 "});"\
+                 "</script>"
+        source
+      else
+        "Error processing input, expected syntax: {% map lat lon zoomlevel markerTitle* markerDescription* %} - * => optional"
+      end
+    end   
+  end
+end
+
+Liquid::Template.register_tag('map', Jekyll::OctoLayer)
